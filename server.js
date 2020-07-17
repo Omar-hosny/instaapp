12a9a5a73539a2d78fa2f24d85a9b5f074402547
omar hosny <omarhosnystar@gmail.com>
2020-07-17 20:15:07 +0200

    added Like & unLike functions backend & frontEnd

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
